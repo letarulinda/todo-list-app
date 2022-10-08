878b0ac0e0d1cbd9d0386f6d5717df91771bd170
letarulinda <letarulinda@gmail.com>
2022-10-08 12:58:14 -0700

    Implement required changes

--- src/index.js
+++ src/index.js
@@ -3,55 +3,6 @@ import { clearAll } from './script.js';
 import taskCompleted from './script.js';
 import './style.css';
 
-const tasks = [
-	{
-		description: 'Go to the GYM',
-		completed: true,
-		index: 0
-	},
-	{
-		description: 'Read ES6 book',
-		completed: true,
-		index: 1
-	},
-	{
-		description: 'Go shopping: ',
-		completed: true,
-		index: 2
-	},
-	{
-		description: 'Wash clothes',
-		completed: true,
-		index: 3
-	},
-	{
-		description: 'Add New Feature to the app',
-		completed: true,
-		index: 4
-	},
-	{
-		description: 'Go camping',
-		completed: true,
-		index: 5
-	},
-	{
-		description: 'Go to church',
-		completed: true,
-		index: 6
-	},
-	{
-		description: 'Cook lunch',
-		completed: true,
-		index: 7
-	},
-	{
-		description: 'Go out',
-		completed: true,
-		index: 8
-	}
-
-]
-
 const myContainer = document.getElementById('to-do-list');
 tasks.forEach((item) => {
 	myContainer.innerHTML += `
